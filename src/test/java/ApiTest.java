0c7fd9c2477f7e30c886a377fca819e186ffd35e
Viktor Frideen <frideen@student.chalmers.se>
2021-11-16 13:20:45 +0100

    Revert "Add Västtrafik API"
    This reverts commit 2fb059df76b62ec4cc5f4499d009360ab630d494.

--- src/test/java/ApiTest.java
+++
@@ -1,8 +0,0 @@
-import okhttp3.Response;
-
-public class ApiTest {
-    public static void main(String[] args) {
-        Vasttrafik vasttrafik = new Vasttrafik();
-        Response response = vasttrafik.getStationsFromCoordinates(11.973372, 57.689953);
-    }
-}
