f84187b77238df2e675d3a66567edbe8f5547cc0
WilmerEriksson <wilmere@student.chalmers.se>
2021-11-17 23:37:31 +0100

    spacetimegraph

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,85 +1,17 @@
-import com.mxgraph.layout.mxCircleLayout;
-import com.mxgraph.layout.mxIGraphLayout;
-import com.mxgraph.swing.mxGraphComponent;
-import org.jgrapht.Graph;
-import org.jgrapht.GraphPath;
-import org.jgrapht.alg.shortestpath.AllDirectedPaths;
-import org.jgrapht.ext.JGraphXAdapter;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.SimpleDirectedWeightedGraph;
-
-import javax.swing.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public final class App {
-    public final static int TIME_DELAY = 10;
-
     public static void main(String[] args) {
-        Graph<VertexData, DefaultWeightedEdge> g = new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class);
-
         FileReader fileReader = new FileReader("data/travel.txt");
         ArrayList<TravelData> travelData = fileReader.getTravelData();
 
-        for (TravelData td : travelData) {
-            g.addVertex(td.getVertexDataStation());
-            g.addVertex(td.getVertexDataDestination());
+        SpaceTimeGraph stGraph = new SpaceTimeGraph(travelData, 10);
+        stGraph.BuildGraph();
 
-            DefaultWeightedEdge edge = g.addEdge(td.getVertexDataStation(), td.getVertexDataDestination());
-            g.setEdgeWeight(edge, td.getWeight());
-        }
+        var paths = stGraph.FindPaths("Korsvägen", "Chalmers");
 
-        for (TravelData td : travelData) {
-            for (TravelData tdd : travelData) {
-                if (td.equals(tdd))
-                    continue;
-
-                if (td.getTime() + td.getWeight() < tdd.getTime() && td.getDestination().equals(tdd.getStation())) {
-                    DefaultWeightedEdge connection = g.addEdge(td.getVertexDataDestination(),
-                            tdd.getVertexDataStation());
-                    g.setEdgeWeight(connection, TIME_DELAY);
-                }
-            }
-        }
-
-        AllDirectedPaths<VertexData, DefaultWeightedEdge> dps = new AllDirectedPaths<>(g);
-        List<GraphPath<VertexData, DefaultWeightedEdge>> allPaths = new ArrayList<>();
-
-        for (VertexData vd : g.vertexSet()) {
-            for (VertexData vdd : g.vertexSet()) {
-                if (vd.equals(vdd))
-                    continue;
-
-                if (!(vd.getName().equals("Korsvägen") && vdd.getName().equals("Chalmers")))
-                    continue;
-
-                if (!vd.getIsSource() || vdd.getIsSource())
-                    continue;
-
-                allPaths.addAll(dps.getAllPaths(vd, vdd, true, null));
-
-            }
-        }
-
-        allPaths.sort(new GraphPathComparator());
-
-        for (GraphPath<VertexData, DefaultWeightedEdge> path : allPaths) {
-            System.out.format("Path: %s, with weight: %f\n", path.getVertexList(), path.getWeight());
-        }
-
-        JFrame frame = new JFrame("DemoGraph");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JGraphXAdapter<VertexData, DefaultWeightedEdge> graphAdapter = new JGraphXAdapter<VertexData, DefaultWeightedEdge>(
-                g);
-
-        mxIGraphLayout layout = new mxCircleLayout(graphAdapter);
-        layout.execute(graphAdapter.getDefaultParent());
-
-        frame.add(new mxGraphComponent(graphAdapter));
-        frame.pack();
-        frame.setLocationByPlatform(true);
-        frame.setVisible(true);
+        stGraph.PrintPaths(paths);
+        stGraph.DrawGraph();
     }
 
 }
