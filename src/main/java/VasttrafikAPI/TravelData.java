009f75f788c334211139aef32e618733c92cf322
Viktor Frideen <frideen@student.chalmers.se>
2021-11-19 09:53:03 +0100

    Add trip request method

--- src/main/java/VasttrafikAPI/TravelData.java
+++ src/main/java/VasttrafikAPI/TravelData.java
@@ -4,6 +4,7 @@ import VasttrafikAPI.ResponseClasses.DepartureBoard.DepartureBoardResponse;
 import VasttrafikAPI.ResponseClasses.JourneyDetail.JourneyDetailResponse;
 import VasttrafikAPI.ResponseClasses.NearbyStops.NearbyStopsResponse;
 import VasttrafikAPI.ResponseClasses.Token;
+import VasttrafikAPI.ResponseClasses.Trip.TripResponse;
 import okhttp3.Request;
 import okhttp3.Response;
 
@@ -49,5 +50,18 @@ public class TravelData {
         Response response = Connection.sendRequest(request);
 
         return JSON.objFromJson(response, JourneyDetailResponse.class);
+    }
+
+    public static TripResponse getTrip(String originId, String destId, Token token) {
+        Map<String, String> auth = new HashMap<>();
+        auth.put("Authorization", "Bearer " + token.getAccessToken());
+
+        Map<String, String> params = new HashMap<>();
+        params.put("originId", originId);
+        params.put("destId", destId);
+
+        Response response = Connection.sendRequest("bin/rest.exe/v2/trip", "GET", params, auth, null);
+
+        return JSON.objFromJson(response, TripResponse.class);
     }
 }
