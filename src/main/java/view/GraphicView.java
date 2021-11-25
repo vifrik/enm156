fe2cc0dbc8e67b234af8df852975670b4559edca
Axel Widerstrand <axelwid@student.chalmers.se>
2021-11-25 19:57:59 +0100

    Updated inteface

--- src/main/java/view/GraphicView.java
+++ src/main/java/view/GraphicView.java
@@ -4,20 +4,23 @@ import model.timetable.MockTimeTable;
 import model.timetable.TimeTable;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GraphicView extends BaseView{
-    private JMenuBar mb;
-
-    private JMenu x;
-
-    private JMenuItem m1, m2, m3;
-
-    // create a frame
-    private JFrame f;
-
-    private JButton b;
-
-    private JSlider s;
+public class GraphicView extends BaseView implements ChangeListener, ActionListener{
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenuItem m1, m2;
+    private JFrame frame;
+    private JButton button;
+    private JSlider slider;
+    private JLabel label;
+    static final int min = 0;
+    static final int max = 10;
+    static final int start = 5;
 
     public GraphicView(TimeTable timeTable) {
         super(timeTable);
@@ -25,43 +28,85 @@ public class GraphicView extends BaseView{
 
     @Override
     protected void setup() {
-        // create a frame
-        f = new JFrame("Menu demo");
+        createFrame();
+        configureFrameExit();
+        setupFrameComponents();
+        sizeFrame();
+        showFrame();
+    }
 
-        // create a menubar
-        mb = new JMenuBar();
+    private void createFrame() {
+        frame = new JFrame();
+    }
 
-        JButton b= new JButton("Sök resa");
-        b.setBounds(50,100,95,30);
-        f.add(b);
-        f.setSize(400,400);
-        f.setLayout(null);
-        f.setVisible(true);
+    private void configureFrameExit() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
-        JSlider s = new JSlider(0,10);
+    private void setupFrameComponents() {
+        //frame
+        frame.setTitle("Västtrafik");
+        frame.setResizable(true);
 
-        // create a menu
-        x = new JMenu("Menu");
+        ImageIcon image = new ImageIcon("buss.png");
+        frame.setIconImage(image.getImage());
 
-        // create menuitems
-        m1 = new JMenuItem("MenuItem1");
-        m2 = new JMenuItem("MenuItem2");
-        m3 = new JMenuItem("MenuItem3");
+        //Menu
+        JMenuItem i1,i2,i3;
+        menuBar = new JMenuBar();
+        menu = new JMenu("Menu");
+        i1 = new JMenuItem("1");
+        i2 = new JMenuItem("2");
+        i3 = new JMenuItem("3");
+        menu.add(i1); menu.add(i2); menu.add(i3);
+        menuBar.add(menu);
+        frame.setJMenuBar(menuBar);
+        frame.add(menuBar);
 
-        // add menu items to menu
-        x.add(m1);
-        x.add(m2);
-        x.add(m3);
+        //Panel
+        JPanel panel1 = new JPanel();
+        panel1.setPreferredSize(new Dimension(400,150));
+        panel1.setBackground(Color.green);
+        panel1.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panel1.setLayout(new GridLayout(0,1));
+        frame.add(panel1, BorderLayout.CENTER);
 
-        // add menu to menu bar
-        mb.add(x);
+        //Panel 2
+        JPanel panel2 = new JPanel();
+        panel2.setPreferredSize(new Dimension(0,50));
+        panel2.setBackground(Color.BLUE);
+        frame.add(panel2, BorderLayout.AFTER_LAST_LINE);
 
-        // add menubar to frame
-        f.setJMenuBar(mb);
+        //Label
+        label = new JLabel("Välj din säkerhets faktor");
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        panel1.add(label);
 
-        // set the size of the frame
-        f.setSize(500, 500);
-        f.setVisible(true);
+        //Slider
+        slider = new JSlider(JSlider.HORIZONTAL,min,max,start);
+        slider.addChangeListener((ChangeListener) this);
+        slider.setMinorTickSpacing(1);
+        slider.setMajorTickSpacing(2);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        panel1.add(slider);
+
+        //button
+        button = new JButton("Sök");
+        button.addActionListener(this);
+        panel2.add(button);
+
+    }
+
+    private void sizeFrame() {
+        frame.setSize(400, 400);
+        frame.pack();
+    }
+
+    private void showFrame() {
+        frame.setVisible(true);
     }
 
     @Override
@@ -77,5 +122,18 @@ public class GraphicView extends BaseView{
         TimeTable timeTable = new MockTimeTable();
         BaseView userInterface = new GraphicView(timeTable);
         userInterface.start();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==button){
+            frame.dispose();
+            System.out.println("Button pressed");
+            NewWindow myWindow = new NewWindow();
+        }
+    }
+
+    public void stateChanged(ChangeEvent e){
+        System.out.println("Slider set to " + slider.getValue());
     }
 }
---
+++ src/main/java/view/NewWindow.java
@@ -0,0 +1,32 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class NewWindow {
+    JFrame frame = new JFrame("Västtrafik");
+    JLabel label = new JLabel();
+
+    NewWindow(){
+        //Label
+        label.setText("Välkommen");
+        label.setBounds(0,100,500,250);
+        label.setFont(new Font(null, Font.PLAIN,25));
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+        frame.add(label);
+
+
+        //Frame
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ImageIcon image = new ImageIcon("buss.png");
+        frame.setIconImage(image.getImage());
+        frame.setSize(500,500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+    }
+}
