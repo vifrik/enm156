8a6ddb55875858317284318c427c7eea07e8ac64
Viktor Frideen <frideen@student.chalmers.se>
2021-12-07 11:12:30 +0100

    Add metric parsing in metric controller

--- src/main/java/controller/MetricController.java
+++ src/main/java/controller/MetricController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MetricController implements IMetricController {
+    public final static String CHANGE_TIME = "change-time";
+    public final static String ADDITIONAL_CHANGE_TIME = "additional-change-time";
     Map<Metric, String> metrics = new HashMap<>();
 
     @Override
@@ -17,12 +19,28 @@ public class MetricController implements IMetricController {
     }
 
     public void setMetric(String metricName, String value) {
-        Metric metric = parseMetric(metricName);
+        Metric metric;
+        switch (metricName) {
+            case CHANGE_TIME -> {
+                metric = Metric.ADDITIONAL_CHANGE_TIME;
+                int v = Integer.parseInt(value);
+                if (v < 5)
+                    throw new IllegalArgumentException("Illegal argument: %d".formatted(v));
+
+                value = String.valueOf(v);
+            }
+            case ADDITIONAL_CHANGE_TIME -> {
+                metric = Metric.ADDITIONAL_CHANGE_TIME;
+                int v = Integer.parseInt(value);
+                if (v < 0)
+                    throw new IllegalArgumentException("Illegal argument: %d".formatted(v));
+            }
+            default -> throw new IllegalArgumentException("Illegal argument: %s".formatted(metricName));
+        }
+
+        metrics.put(metric, value);
     }
 
-    private Metric parseMetric(String metricName) {
-        return switch (metricName) {
-            case "change" -> Metric.ADDITIONAL_CHANGE_TIME;
-        }
+    private record MetricValuePair(Metric metric, String value) {
     }
 }
