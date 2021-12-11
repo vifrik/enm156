b4dcf8cc31682d9a052514c251110d766e79ce55
Artur Gasparyan <gasparyanartur99@gmail.com>
2021-12-11 16:49:58 +0100

    Fix styling on metric menu

--- src/main/java/view/graphicview/MenuWindow.java
+++ src/main/java/view/graphicview/MenuWindow.java
@@ -6,74 +6,25 @@ import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 
 
 public class MenuWindow implements  ChangeListener, ActionListener {
-    JFrame frame = new JFrame("Västtrafik");
-    JLabel label = new JLabel();
-    JSlider slider;
-    JMenu menu;
-    JMenuItem menuItem;
-    JMenuBar menuBar;
+    JFrame frame;
+    JSlider changeTimeSlider;
     JButton helpButton, backButton;
     private final GraphicView graphicView;
 
     MenuWindow(GraphicView graphicView){
         this.graphicView = graphicView;
-        JPanel panel = new JPanel();
-        panel.setPreferredSize(new Dimension(0,50));
-        panel.setBackground(Color.BLUE);
-        frame.add(panel, BorderLayout.AFTER_LAST_LINE);
 
-        //Menu
-        menuBar = new JMenuBar();
-        menu = new JMenu("Meny");
-        menuBar.add(menu);
+        createFrame();
+        createPanel();
+        createChangeTimeSlider();
+        createControlButtons();
+    }
 
-        menuItem = new JMenuItem("Sök",
-                KeyEvent.VK_T);
-        menuItem.addActionListener(this);
-        menu.add(menuItem);
-        frame.setJMenuBar(menuBar);
-
-
-
-        //Label
-        label.setText("Välj säkerhetsfaktor");
-        label.setBounds(100,100,300,50);
-        label.setFont(new Font(null, Font.PLAIN,15));
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalAlignment(JLabel.CENTER);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        frame.add(label);
-
-
-        //Slider
-        slider = new JSlider(JSlider.HORIZONTAL,5,60,5);
-        slider.setBounds(100,150,300,100);
-        slider.addChangeListener(this);
-        slider.setMinorTickSpacing(1);
-        slider.setMajorTickSpacing(2);
-        slider.setPaintTicks(true);
-        slider.setPaintLabels(true);
-        frame.add(slider);
-
-        //Button
-        helpButton = new JButton("Hjälp");
-        helpButton.setBounds(200,250,100,40);
-        helpButton.addActionListener(this);
-        frame.add(helpButton);
-
-        backButton = new JButton("Tillbaka");
-        backButton.setBounds(200, 300, 100, 40);
-        backButton.addActionListener(this);
-        frame.add(backButton);
-
-        //Pop-up
-
-        //Frame
-        //frame.setResizable(false);
+    private void createFrame() {
+        frame = new JFrame("Västtrafik");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ImageIcon image = new ImageIcon("buss.png");
         frame.setIconImage(image.getImage());
@@ -81,30 +32,62 @@ public class MenuWindow implements  ChangeListener, ActionListener {
         frame.setLayout(null);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
 
+    private void createPanel() {
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500, 500));
+        frame.add(panel, BorderLayout.AFTER_LAST_LINE);
+    }
+
+    private void createChangeTimeSlider() {
+        changeTimeSlider = new JSlider(JSlider.HORIZONTAL,5,60,5);
+        changeTimeSlider.setBounds(200,100,180,50);
+        changeTimeSlider.addChangeListener(this);
+        changeTimeSlider.setMinorTickSpacing(1);
+        changeTimeSlider.setMajorTickSpacing(5);
+        changeTimeSlider.setPaintTicks(true);
+        changeTimeSlider.setPaintLabels(true);
+
+        var label = new JLabel();
+        label.setText("Bytestid");
+        label.setBounds(100,100,100,50);
+        label.setFont(new Font(null, Font.PLAIN,15));
+        label.setHorizontalTextPosition(JLabel.LEFT);
+        label.setVerticalAlignment(JLabel.CENTER);
+
+        frame.add(changeTimeSlider);
+        frame.add(label);
+    }
+
+    private void createControlButtons() {
+        helpButton = createButton("Hjälp", new Rectangle(200, 250, 100, 40));
+        backButton = createButton("Tillbaka", new Rectangle(200, 300, 100, 40));
+    }
+
+    private JButton createButton(String title, Rectangle bounds) {
+        var button = new JButton(title);
+        button.setBounds(bounds);
+        button.addActionListener(this);
+        frame.add(button);
+        return button;
     }
 
     @Override
     public void stateChanged(ChangeEvent e) {
-        System.out.println("Slider set to " + slider.getValue());
-        graphicView.metricController.setMetric("change-time", Integer.toString(slider.getValue()));
+        if (e.getSource() == changeTimeSlider) {
+            graphicView.metricController.setMetric("change-time", Integer.toString(changeTimeSlider.getValue()));
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==menuItem){
-            frame.dispose();
-            //GraphicView graphicView = new GraphicView();
-        }
-        if(e.getSource()== helpButton){
-            JOptionPane optionPane= new JOptionPane();
-            JOptionPane.showMessageDialog(frame,"Genom att ställa in säkerhetsfaktorn väljer du hur säker din resa ska vara samt hur mycket du är redo att riskera för snabbhet");
+        if (e.getSource()== helpButton){
+            JOptionPane.showMessageDialog(frame,"Bytestid: Minsta antalet minuter ett byte ska räknas ta");
         }
         if (e.getSource() == backButton) {
             graphicView.frame.setVisible(true);
             frame.dispose();
         }
-
-
     }
 }
